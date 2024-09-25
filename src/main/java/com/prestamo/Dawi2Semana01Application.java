cbcb0136068c285c670f5424e6ed7ea689057a66
Peito1102 <renzitiwin@gmail.com>
2024-09-25 18:13:07 -0500

    primer commit listado de paises

---
+++ src/main/java/com/prestamo/Dawi2Semana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawi2Semana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawi2Semana01Application.class, args);
+	}
+
+}
